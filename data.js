459fd05ee0d62fbec3efad7352947c1b76bb9e71
Dmitry Kiselyov <dkiselyov@evolution.com>
2024-04-09 10:09:23 +0200

    Add section images

--- data.js
+++ data.js
@@ -1,32 +1,52 @@
+/*
+ * 1. Upload an image to https://imgbb.com/
+ * 2. Take the link of the uploaded image (HTML full linked => <img src="{NEEDED LINK}" ...>)
+ * 3. Insert the link in the `img` field where necessary or change the `siteLogo`
+ */
+
+const siteLogo = "https://i.ibb.co/3sz0L5r/logo.png";
+
 const sections = {
-  "IP-видеокамеры": [
-    ["196.168.1.1", "My name 1"],
-    ["196.168.1.2", "My name 2"],
-    ["196.168.1.3", "My name 3"],
-    ["196.168.1.4", "My name 4"],
-    ["196.168.1.5", "My name 5"],
-    ["196.168.1.6", "My name 6"],
-    ["196.168.1.7", "My name 7"],
-    ["196.168.1.8", "My name 8"],
-    ["196.168.1.9", "My name 9"],
-    ["196.168.1.10", "My name 10"],
-    ["196.168.1.11", "My name 11"],
-    ["196.168.1.12", "My name 12"],
-  ],
-  "Контроллеры управления": [
-    ["196.168.1.1", "My name 1"],
-    ["196.168.1.2", "My name 2"],
-    ["196.168.1.3", "My name 3"],
-  ],
-  "Маршрутизаторы": [
-    ["196.168.1.1", "My name 1"],
-    ["196.168.1.2", "My name 2"],
-    ["196.168.1.3", "My name 3"],
-    ["196.168.1.4", "My name 4"],
-  ],
-  "Контроллеры СКУД": [
-    ["196.168.1.1", "My name 1"],
-    ["196.168.1.2", "My name 2"],
-    ["196.168.1.3", "My name 3"],
-  ],
+  "IP-видеокамеры": {
+    img: "https://i.ibb.co/2gjBTG5/camera.png",
+    ips: [
+      ["196.168.1.1", "My name 1"],
+      ["196.168.1.2", "My name 2"],
+      ["196.168.1.3", "My name 3"],
+      ["196.168.1.4", "My name 4"],
+      ["196.168.1.5", "My name 5"],
+      ["196.168.1.6", "My name 6"],
+      ["196.168.1.7", "My name 7"],
+      ["196.168.1.8", "My name 8"],
+      ["196.168.1.9", "My name 9"],
+      ["196.168.1.10", "My name 10"],
+      ["196.168.1.11", "My name 11"],
+      ["196.168.1.12", "My name 12"],
+    ],
+  },
+  "Контроллеры управления": {
+    img: "https://i.ibb.co/2gjBTG5/camera.png",
+    ips: [
+      ["196.168.1.1", "My name 1"],
+      ["196.168.1.2", "My name 2"],
+      ["196.168.1.3", "My name 3"],
+    ],
+  },
+  "Маршрутизаторы": {
+    img: "https://i.ibb.co/2gjBTG5/camera.png",
+    ips: [
+      ["196.168.1.1", "My name 1"],
+      ["196.168.1.2", "My name 2"],
+      ["196.168.1.3", "My name 3"],
+      ["196.168.1.4", "My name 4"],
+    ],
+  },
+  "Контроллеры СКУД": {
+    img: "https://i.ibb.co/2gjBTG5/camera.png",
+    ips: [
+      ["196.168.1.1", "My name 1"],
+      ["196.168.1.2", "My name 2"],
+      ["196.168.1.3", "My name 3"],
+    ],
+  },
 };
--- index.js
+++ index.js
@@ -1,35 +1,47 @@
 const ipAddressRegExp = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
 
+const mainLogoElement = document.getElementById('siteLogo');
+mainLogoElement.src = siteLogo || "";
+
 const mainElement = document.getElementsByTagName('main')[0];
 
-/** @type {Record<string, [ip: string, name: string][]>} */
+/** @type {Record<string, { img?: string; ips: [ip: string, name: string][]>}} */
 const allSections = sections || [];
 
 Object.entries(allSections).forEach(([key, sectionData]) => {
+  const { img, ips } = sectionData;
+
   const sectionElement = document.createElement('div');
   sectionElement.className = 'section';
+
+  if (img) {
+    const imgElement = document.createElement('img');
+    imgElement.src = img;
+    imgElement.alt = key;
+    imgElement.className = 'sectionLogo';
+    sectionElement.appendChild(imgElement);
+  }
 
   const titleElement = document.createElement('p');
   titleElement.className = 'title'
   titleElement.innerText = `${key}:`;
   sectionElement.appendChild(titleElement);
 
+
   const listElement = document.createElement('ol');
-  sectionData.forEach(([ip, name]) => {
+  ips.forEach(([ip, name]) => {
     const ipElement = document.createElement('li');
     ipElement.innerHTML = `<b>${ip}</b> - ${name}`;
 
-    const goToButton = document.createElement('button');
-    goToButton.addEventListener('click', () => {
-      if (ipAddressRegExp.test(ip)) {
-        window.location.href = `http://${ip}/`
-      } else {
-        window.alert('IP-адрес не действительный.');
-      }
-    });
+    if (!ipAddressRegExp.test(ip)) {
+      console.error(`IP-адрес "${ip}" не действительный.`);
+    }
 
-    goToButton.className = 'goTo'
-    goToButton.innerText = '➡️';
+    const goToButton = document.createElement('a');
+    goToButton.target = '_blank';
+    goToButton.href = `http://${ip}/`;
+    goToButton.className = 'goTo';
+    goToButton.innerText = 'Подключиться';
     ipElement.appendChild(goToButton);
 
     listElement.appendChild(ipElement);
